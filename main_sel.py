2c6515a7e2f007f731613e87a78cd2977bd12627
Denis Serdyuk <33402077+diserdyuk@users.noreply.github.com>
2020-12-01 18:40:50 +0200

    Add files via upload

--- main_sel.py
+++ main_sel.py
@@ -8,12 +8,6 @@ import itertools
 
 
 
-def write_csv(d):
-    with open('iwilltravel.csv', 'a') as f:
-        write = csv.writer(f)
-        write.writerow(d)
-
-
 def get_page(data):
     soup = BeautifulSoup(data, 'lxml')
 
@@ -26,8 +20,7 @@ def get_page(data):
             title_l.append(title)
         except:
             title = ''
-        # print(type(title))
-    # print(t)
+    # print(title_l)
  
     for link in titles:    # get links on page
         try:
@@ -35,22 +28,21 @@ def get_page(data):
             link_l.append(link)
         except:
             link = ''    
-        # print(link)
-    # print(l)
+    # print(link_l)
 
     category_l = [ ]
     locate_l = [ ]
     location = soup.find_all('span', class_='term')
     cnt = 1
     for i in location:
-        if cnt % 2 != 0:    # было -> if cnt / 2 != 0:
+        if cnt % 2 != 0:
             try:
                 category = i.text
                 category_l.append(category)
                 # print(cnt, category)
             except:
                 category = ''          
-        if cnt % 2 == 0:    # было -> if cnt / 2 == 0:
+        if cnt % 2 == 0:
             try:
                 locate = i.text
                 locate_l.append(locate)
@@ -64,7 +56,7 @@ def get_page(data):
     for k, l, m, n in zip(title_l, link_l, category_l, locate_l): 
         data = (k, l, m, n)
         
-        with open("iwilltravl5.csv", "a", newline="") as file:
+        with open("iwilltravl7.csv", "a", newline="") as file:
             writer = csv.writer(file)
             writer.writerow(data)
 
@@ -73,14 +65,25 @@ def get_page(data):
 def main():
     url = 'https://iwilltravelagain.com/usa/?page=1'
     
-
     driver = webdriver.Chrome()   #executable_path='chromedriver')
+    time.sleep(8)
     driver.get(url)    # open page
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")    # scroll page from top to down
-    time.sleep(6)
-    page = driver.page_source    # get html code
+    time.sleep(40)
 
-    get_page(page)
+    cnt = 1
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")    # scroll page from top to down
+        time.sleep(40)
+
+        page = driver.page_source    # get html code
+        get_page(page)
+        time.sleep(6)
+
+        driver.find_element_by_xpath('/html/body/main/section[3]/div[2]/div[4]/button[8]').click()
+        time.sleep(300)
+        cnt += 1
+        if cnt == 294:
+            break
 
 
 
